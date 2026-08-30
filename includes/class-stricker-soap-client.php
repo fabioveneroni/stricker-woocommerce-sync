b80ad4a2d69c35bc3e37caf456fed559d3b78093
fabioveneroni <159145140+fabioveneroni@users.noreply.github.com>
2026-08-30 23:15:59 +0100

    v0.4.1 - Add official ProductsTree SOAP method

--- includes/class-stricker-soap-client.php
+++ includes/class-stricker-soap-client.php
@@ -7,45 +7,18 @@ class StrickerSoapClient {
     public static $protocol;
     public static $clientWCFAZURE;
 
-    public function __construct( $protocol = 'https' ) {
-        self::$protocol = ( 'http' === strtolower( $protocol ) ) ? 'http' : 'https';
-    }
-
+    public function __construct( $protocol = 'https' ) { self::$protocol = ( 'http' === strtolower( $protocol ) ) ? 'http' : 'https'; }
     public function StrickerSOAPClient( $protocol ) { $this->__construct( $protocol ); }
 
     public static function InitializeSoap() {
         if ( self::$clientWCFAZURE === null ) {
             $wsdl = ( 'http' === self::$protocol ) ? self::$_WsdlUriHTTP : self::$_WsdlUriHTTPS;
-            self::$clientWCFAZURE = new SoapClient( $wsdl, array(
-                'cache_wsdl' => WSDL_CACHE_NONE,
-                'connection_timeout' => 120,
-                'exceptions' => true,
-                'trace' => false,
-            ) );
+            self::$clientWCFAZURE = new SoapClient( $wsdl, array('cache_wsdl'=>WSDL_CACHE_NONE,'connection_timeout'=>120,'exceptions'=>true,'trace'=>false) );
         }
     }
-
-    public static function AuthenticateClient( $accessKey ) {
-        self::InitializeSoap();
-        $response = self::$clientWCFAZURE->AuthenticateClient( array( 'accessKey' => $accessKey ) );
-        return $response->AuthenticateClientResult;
-    }
-
-    public static function ValidateSession( $token ) {
-        self::InitializeSoap();
-        $response = self::$clientWCFAZURE->ValidateSession( array( 'token' => $token ) );
-        return $response->ValidateSessionResult;
-    }
-
-    public static function ProductTypes( $token, $language ) {
-        self::InitializeSoap();
-        $response = self::$clientWCFAZURE->ProductTypes( array( 'token' => $token, 'lang' => $language ) );
-        return $response->ProductTypesResult;
-    }
-
-    public static function Products( $token, $language ) {
-        self::InitializeSoap();
-        $response = self::$clientWCFAZURE->Products( array( 'token' => $token, 'lang' => $language ) );
-        return $response->ProductsResult;
-    }
+    public static function AuthenticateClient( $accessKey ) { self::InitializeSoap(); $r=self::$clientWCFAZURE->AuthenticateClient(array('accessKey'=>$accessKey)); return $r->AuthenticateClientResult; }
+    public static function ValidateSession( $token ) { self::InitializeSoap(); $r=self::$clientWCFAZURE->ValidateSession(array('token'=>$token)); return $r->ValidateSessionResult; }
+    public static function ProductTypes( $token, $language ) { self::InitializeSoap(); $r=self::$clientWCFAZURE->ProductTypes(array('token'=>$token,'lang'=>$language)); return $r->ProductTypesResult; }
+    public static function Products( $token, $language ) { self::InitializeSoap(); $r=self::$clientWCFAZURE->Products(array('token'=>$token,'lang'=>$language)); return $r->ProductsResult; }
+    public static function ProductsTree( $token, $language ) { self::InitializeSoap(); $r=self::$clientWCFAZURE->ProductsTree(array('token'=>$token,'lang'=>$language)); return $r->ProductsTreeResult; }
 }
