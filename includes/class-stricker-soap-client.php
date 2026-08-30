7563f2eb59b8b8c58353ea25c0b7579fe6682c0d
fabioveneroni <159145140+fabioveneroni@users.noreply.github.com>
2026-08-30 22:02:57 +0100

    v0.3.4 - Add Stricker SOAP client wrapper

---
+++ includes/class-stricker-soap-client.php
@@ -0,0 +1,47 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class StrickerSoapClient {
+    public static $_WsdlUriHTTP = 'http://ws.spotgifts.com.br/StrickerService.svc?wsdl';
+    public static $_WsdlUriHTTPS = 'https://ws.spotgifts.com.br/StrickerService.svc?wsdl';
+    public static $protocol;
+    public static $clientWCFAZURE;
+
+    public function __construct( $protocol = 'https' ) {
+        self::$protocol = ( 'http' === strtolower( $protocol ) ) ? 'http' : 'https';
+    }
+
+    public function StrickerSOAPClient( $protocol ) {
+        $this->__construct( $protocol );
+    }
+
+    public static function InitializeSoap() {
+        if ( self::$clientWCFAZURE === null ) {
+            $wsdl = ( 'http' === self::$protocol ) ? self::$_WsdlUriHTTP : self::$_WsdlUriHTTPS;
+            self::$clientWCFAZURE = new SoapClient( $wsdl, array(
+                'cache_wsdl' => WSDL_CACHE_NONE,
+                'connection_timeout' => 120,
+                'exceptions' => true,
+                'trace' => false,
+            ) );
+        }
+    }
+
+    public static function AuthenticateClient( $accessKey ) {
+        self::InitializeSoap();
+        $response = self::$clientWCFAZURE->AuthenticateClient( array( 'accessKey' => $accessKey ) );
+        return $response->AuthenticateClientResult;
+    }
+
+    public static function ValidateSession( $token ) {
+        self::InitializeSoap();
+        $response = self::$clientWCFAZURE->ValidateSession( array( 'token' => $token ) );
+        return $response->ValidateSessionResult;
+    }
+
+    public static function ProductTypes( $token, $langage ) {
+        self::InitializeSoap();
+        $response = self::$clientWCFAZURE->ProductTypes( array( 'token' => $token, 'lang' => $langage ) );
+        return $response->ProductTypesResult;
+    }
+}
